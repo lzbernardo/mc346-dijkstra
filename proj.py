386eb14d909507bd22fc7df426018d84717fb354
lzbernardo <bernardo.lanza@gmail.com>
2019-11-06 15:17:11 -0300

    input reading

--- proj.py
+++ proj.py
@@ -1,34 +1,44 @@
-import sys, fileinput
+import sys, fileinput, collections
 
 def main():
     fileInput = fileinput.input()
-    graph = {}
-    # reads first line
-    defaultSpeed = input()
+    graph = collections.defaultdict(dict)
+    distgraph = collections.defaultdict(dict)
+    # reads first line (default speed)
+    defaultSpeed = float(input())
 
     # reads graph adjacency list
     for line in fileInput:
-        if line == "\n":
-            break
-        if line.count(" ") < 3:
-            node0, node1, dist = map(str, line.split())
-            maxSpd = defaultSpeed
+        if len(line) > 2:
+            if line.count(" ") < 3:
+                node0, node1, dist = map(str, line.split())
+                maxSpd = defaultSpeed
+            else:
+                node0, node1, dist, maxSpd = map(str, line.split())
+            graph[node0][node1] = [float(maxSpd)]
+            distgraph[node0][node1] = float(dist)
         else:
-            node0, node1, dist, maxSpd = map(str, line.split())
+            break
 
-        print(node0 + ' ' + node1 + ' ' + dist + ' ' + maxSpd)
-        # splitted = list(map(str, line.split()))
-        # node0 = splitted[0]
-        # node1 = splitted[1]
-        # dist = splitted[2]
-        # maxSpd = defaultSpeed
-        # if len(splitted) > 3:
-        #     maxSpd = splitted[3]
 
-    print("heeeey")
-    # reads velocities (?)
+    graph['aa']['b'].append(69.0)
+    print(graph['aa'])
+
+    path = []
+    # reads waze velocities
     for line in fileInput:
-        print(line)
+        if line.count(" ") > 0:
+            spl = line.split()
+            node0 = spl[0]
+            node1 = spl[1]
+            for i in range(2,len(spl)):
+                graph[node0][node1].append(float(spl[i]))
+        else:
+            spl = line.split()
+            path.append(spl[0])
+
+    print(path)
+    print(graph)
 
 
 if __name__ == '__main__':
