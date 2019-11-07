ec9aa5c7a1c1109682540696dfc7495b6acb053a
lzbernardo <bernardo.lanza@gmail.com>
2019-11-06 22:04:22 -0300

    dep

--- proj.py
+++ proj.py
@@ -1,31 +1,21 @@
-import sys, fileinput, collections
+import fileinput, random
+from heapq import *
+from collections import defaultdict
 
 def main():
     fileInput = fileinput.input()
-    graph = collections.defaultdict(dict)
-    distgraph = collections.defaultdict(dict)
-    # reads first line (default speed)
-    defaultSpeed = float(input())
+    graph = defaultdict(dict)
+    distgraph = defaultdict(list)
+    path = []
+    avgs = set([])
 
-    # reads graph adjacency list
+    defaultSpeed = float(input())
     for line in fileInput:
         if len(line) > 2:
-            if line.count(" ") < 3:
-                node0, node1, dist = map(str, line.split())
-                maxSpd = defaultSpeed
-            else:
-                node0, node1, dist, maxSpd = map(str, line.split())
-            graph[node0][node1] = [float(maxSpd)]
-            distgraph[node0][node1] = float(dist)
+            readNode(line, graph, distgraph, defaultSpeed)
         else:
             break
 
-
-    graph['aa']['b'].append(69.0)
-    print(graph['aa'])
-
-    path = []
-    # reads waze velocities
     for line in fileInput:
         if line.count(" ") > 0:
             spl = line.split()
@@ -37,9 +27,72 @@ def main():
             spl = line.split()
             path.append(spl[0])
 
-    print(path)
-    print(graph)
+    # print("\n== iterations ==")
+    for i in range(0,100):
+        # print('\n=> it ', end="")
+        # print(i+1)
+        gs = sample(graph) # adjacency list w/ sample max speed
+        tgs = timefy(gs, distgraph) # multiplies distances with max speed and creates a graph with 'time' as key
+        sp = dijkstra(tgs, path[0], path[1])
+        if sp != float("inf"):
+            avgs.add(sp)
 
+    avgs = sorted(list(avgs), key=lambda x: x[0])
+
+    if len(avgs) > 0:
+        res1 = avgs[0]
+        print("{:.1f}".format(res1[0]))
+        print(' '.join(res1[1]))
+    if len(avgs) > 1:
+        res2 = avgs[1]
+        print("{:.1f}".format(res2[0]))
+        print(' '.join(res2[1]))
+
+
+def readNode(line, graph, distgraph, defSpeed):
+    if line.count(" ") < 3:
+        node0, node1, dist = map(str, line.split())
+        maxSpd = defSpeed
+    else:
+        node0, node1, dist, maxSpd = map(str, line.split())
+    graph[node0][node1] = [float(maxSpd)]
+    distgraph[node0].append((float(dist),node1))
+    # distgraph[node0][node1] = float(dist)
+
+def dijkstra(g, f, t):
+    q, seen, mins = [(0,f,())], set(), {f: 0}
+    while q:
+        (cost,v1,path) = heappop(q)
+        if v1 not in seen:
+            seen.add(v1)
+            path += (v1,)
+            if v1 == t: return (cost, path)
+
+            for c, v2 in g.get(v1, ()):
+                if v2 in seen: continue
+                prev = mins.get(v2, None)
+                next = cost + c
+                if prev is None or next < prev:
+                    mins[v2] = next
+                    heappush(q, (next,v2,path))
+    return float("inf")
+
+def sample(g):
+    r = defaultdict(dict)
+    for k, subdict in g.items():
+        for sk, value in subdict.items():
+            rv = random.choice(value)
+            r[k][sk] = rv
+    return r
+
+def timefy(g, dg):
+    tg = defaultdict(list)
+    for key, value in dg.items():
+        for dist, dest in value:
+            time = g[key][dest]
+            if (time*dist) != 0:
+                tg[key].append((time*dist, dest))
+    return tg
 
 if __name__ == '__main__':
     main()
